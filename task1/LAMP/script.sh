b612cc9555dd5e0859eee179ce403ae3006206c8
Teos WorkStation <teos@otaku.gmail.com>
2022-03-23 17:19:36 +0300

    make new sh for LAMP

--- task1/LAMP/script.sh
+++ task1/LAMP/script.sh
@@ -1,61 +1,29 @@
 #!/bin/bash
 
-sudo su
-cd
-#mysql
-#echo ">>> Installing MySQL 5.7"
-yum -y remove mariadb*
-tee -a /etc/yum.repos.d/mysql-community.repo << END
-[mysql57-community]
-name=MySQL 5.7 Community Server
-baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/\$basearch/
-enabled=1
-gpgcheck=0
-END
-yum -y -q update
-yum -y -q install mysql-community-{server,client,common,libs}-* --exclude='*minimal*'
+sudo dnf update
+sudo dnf install dnf-utils
 
-systemctl start mysqld
-systemctl enable mysqld
+#install Apache
 
-tempPassword="`grep 'temporary.*root@localhost' /var/log/mysqld.log | sed 's/.*root@localhost: //'`"
-newPassword="mySQL@123"
+sudo dnf install httpd httpd-tools
+sudo systemctl enable httpd
+sudo systemctl start httpd
 
-echo -e "[client]\nuser=\"root\"\npassword=\"$tempPassword\"" >> ~/.my.cnf
 
-mysql --connect-expired-password -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY '$newPassword';"
-mysql -e "uninstall plugin validate_password;"
+#install php
 
-sed -i "s/$tempPassword/$newPassword/" ~/.my.cnf
+sudo dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+sudo dnf module reset php
+sudo dnf module enable php:remi-7.4
+sudo dnf install php php-cli php-mysqlnd
+dnf install php-gd php-zip php-mcrypt php-json php-xml php-gettext php-curl php-intl 
 
-systemctl restart mysqld.service
-systemctl enable mysqld.service
 
-mysql -e "FLUSH PRIVILEGES;"
+#install MySQL
 
-mysql -e "CREATE DATABASE wordpress_db;"
-mysql -e "CREATE USER wordpress_user@localhost IDENTIFIED BY 'wordpress';"
-mysql -e "GRANT ALL ON wordpress_db.* to wordpress_user@localhost;"
-mysql -e "FLUSH PRIVILEGES;"
+sudo dnf -y install @mysql
+sudo systemctl enable mysqld.service
+sudo systemctl start mysqld.service
 
-#apache
-#echo ">>> Installing Apache"
-yum -y -q install httpd
-systemctl start httpd
-systemctl enable httpd
 
-#php
-#echo ">>> Installing PHP 7.4"
-yum -y install yum-utils epel-release
-yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum -y --enablerepo=remi-php74 install httpd epel-release yum-utils php php-fpm php-gd php-pdo php-mbstring php-pear php-mysql
-
-systemctl stop httpd
-
-# rm -rf /var/www/html
-ln -fs /vagrant/public /var/www/html
-
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php.ini
-
-systemctl restart httpd
+sudo systemctl restart httpd

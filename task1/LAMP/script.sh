59490249f02d20b3e83c37dc2394cd9d264c55ff
Teos WorkStation <teos@otaku.gmail.com>
2022-03-23 17:38:10 +0300

    fix sh

--- task1/LAMP/script.sh
+++ task1/LAMP/script.sh
@@ -5,17 +5,17 @@ sudo dnf install dnf-utils
 
 #install Apache
 
-sudo dnf install httpd httpd-tools
+sudo dnf install httpd httpd-tools -y
 sudo systemctl enable httpd
 sudo systemctl start httpd
 
 
 #install php
 
-sudo dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+sudo dnf install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm 
 sudo dnf module reset php
 sudo dnf module enable php:remi-7.4
-sudo dnf install php php-cli php-mysqlnd
+sudo dnf install -y php php-cli php-mysqlnd
 dnf install php-gd php-zip php-mcrypt php-json php-xml php-gettext php-curl php-intl 
 
 
